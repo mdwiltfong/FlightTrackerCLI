9834e5888f36bf58a2db40469f28af62978cde32
DeathAdderR <superdade1992@gmail.com>
2024-12-09 19:30:03 -0600

    Add axios and implement in FlightTrackerCLI class

--- bin/utils/utils.js
+++ bin/utils/utils.js
@@ -1,7 +1,38 @@
-class FlightTrackerCLI {
-  constructor() {}
 
-  login(manualLogin = false, userName = "", passWord = "") {}
+const axios = require("axios");
+
+class FlightTrackerCLI {
+  constructor() {
+    this.axiosInstance = axios.create({
+      baseURL: 'https://opensky-network.org/api',
+      timeout: 5000,
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+  }
+
+  async findFlights(iataCode) {
+    try {
+      const response = await this.axiosInstance.get('flights', {
+        params: {
+          airport: iataCode,
+        },
+      });
+      console.log(response.data)
+    } catch (error) {
+      console.error('Error fetching flights:', error);
+    }
+  }
+
+  async login(manualLogin = false, userName = "", passWord = "") {
+    if (manualLogin) {
+      // manual logic
+    }
+    else {
+      // use axios to fetch login-related data
+    }
+  }
 }
 
 module.exports = FlightTrackerCLI;
