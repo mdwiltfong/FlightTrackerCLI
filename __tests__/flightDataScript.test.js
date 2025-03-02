5911813c69d5304c128de4659afc47d90fc109b4
Michael Wiltfong <76107997+mdwiltfong@users.noreply.github.com>
2025-03-02 13:11:48 -0500

    Implement Finding an airport's IATA code Fixes #12

--- __tests__/flightDataScript.test.js
+++ __tests__/flightDataScript.test.js
@@ -1,18 +1,23 @@
 const fs = require("node:fs/promises");
+const path = require("node:path");
 const generateAirPortData = require("../flightData/scripts/index");
-console.log(generateAirPortData);
 describe("Airports.json file", () => {
+  const filePath = path.resolve(__dirname, "../Airports.json");
   beforeAll(async () => {
     try {
-      await fs.unlink(path.resolve(__dirname, "../Airports.json"));
+      await fs.unlink(filePath);
     } catch (error) {
       if (error.code !== "ENOENT") {
         throw new Error(error);
       }
     }
   });
+  afterAll(async () => {
+    fs.unlink(filePath);
+  });
   it("should contain data", async () => {
     await generateAirPortData();
+    const airportsJSON = await fs.readFile(filePath);
     expect(airportsJSON).toBeDefined();
     expect(Object.keys(airportsJSON).length).toBeGreaterThan(0);
   });
--- flightData/scripts/index.js
+++ flightData/scripts/index.js
@@ -3,35 +3,41 @@ const readline = require("node:readline");
 const hashMap = new Map();
 
 module.exports = async function Main() {
-  const fileStream = fs.createReadStream("./flightData/RawFlightData.txt", {
-    encoding: "utf8",
-  });
-  const writeStream = fs.createWriteStream("./Airports.json");
+  return new Promise((resolve, reject) => {
+    const fileStream = fs.createReadStream("./flightData/RawFlightData.txt", {
+      encoding: "utf8",
+    });
+    const writeStream = fs.createWriteStream("./Airports.json");
 
-  const rl = readline.createInterface({
-    input: fileStream,
-    output: writeStream,
-  });
-  rl.on("line", (line) => {
-    const lineArray = line.split(",");
-    const airportName = lineArray[1].slice(1, lineArray[1].length - 1);
-    const IATACode = lineArray[4].slice(1, lineArray[4].length - 1);
-    const airport = {
-      airportName: airportName,
-      IATACode: IATACode,
-    };
-    if (hashMap.has(airportName[0])) {
-      const airports = hashMap.get(airportName[0]);
-      airports.push(airport);
-      hashMap.set(airportName[0], airports);
-      return;
-    }
-    hashMap.set(airportName[0], [airport]);
-  });
-  rl.on("close", () => {
-    const obj = Object.fromEntries(hashMap);
-    const json = JSON.stringify(obj, null, 2); // Pretty print with 2 spaces
-    writeStream.write(json);
-    writeStream.end();
+    const rl = readline.createInterface({
+      input: fileStream,
+      output: writeStream,
+    });
+    rl.on("line", (line) => {
+      const lineArray = line.split(",");
+      const airportName = lineArray[1].slice(1, lineArray[1].length - 1);
+      const IATACode = lineArray[4].slice(1, lineArray[4].length - 1);
+      const airport = {
+        airportName: airportName,
+        IATACode: IATACode,
+      };
+      if (hashMap.has(airportName[0])) {
+        const airports = hashMap.get(airportName[0]);
+        airports.push(airport);
+        hashMap.set(airportName[0], airports);
+        return;
+      }
+      hashMap.set(airportName[0], [airport]);
+    });
+    rl.on("close", () => {
+      const obj = Object.fromEntries(hashMap);
+      const json = JSON.stringify(obj, null, 2); // Pretty print with 2 spaces
+      writeStream.write(json);
+      writeStream.end();
+      resolve();
+    });
+    rl.on("error", (err) => {
+      reject(err);
+    });
   });
 };
